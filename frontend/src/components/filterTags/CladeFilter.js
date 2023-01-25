0a7474782761f53744989b0ac94f7516407fdd1a
thomasxwright <wright.ptw@gmail.com>
2023-01-25 17:49:17 -0600

    SUPER tiny push, setting the width of one of the filter tags so it doesn't widen the whole page when viewed on my S10e screen.

--- frontend/src/components/filterTags/CladeFilter.js
+++ frontend/src/components/filterTags/CladeFilter.js
@@ -2,10 +2,10 @@ import CladeSelect from './CladeSelect'
 
 const CladeFilter = ({ clade, setClade }) => {
     return (
-        <>
+        <div style={{width: '100%'}}>
             <CladeSelect clade={clade} setClade={setClade}/>
             <input type="text" defaultValue={clade.cladeInput} onChange={e => setClade({...clade, cladeInput: e.target.value})}/>
-        </>
+        </div>
     )
 }
 
